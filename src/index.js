22fae228d68ff13df2ac6451be703f7cd91fbf93
Weslley Araujo <weslleyaraujo20@gmail.com>
2017-03-14 23:43:57 +0100

    refactor(*): dead files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <h1>Nothing to see here</h1>,
   document.getElementById('root')
 );
